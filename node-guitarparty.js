16c87da3ffe784052a9b4eda3c1773293b431e8d
tbroadley <buriedunderbooks@hotmail.com>
2016-05-31 11:32:25 +0100

    implement client

---
+++ node-guitarparty.js
@@ -0,0 +1,35 @@
+const http = require('http');
+
+function Guitarparty(apiKey) {
+  this.apiKey = apiKey;
+};
+
+Guitarparty.prototype.request = function(endpoint, callback) {
+  const options = {
+    hostname: 'api.guitarparty.com',
+    path: `/v2/${endpoint}`,
+    headers: {
+      'Guitarparty-Api-Key': this.apiKey
+    },
+  };
+
+  let req = http.request(options, res => {
+    let body = '';
+
+    res.setEncoding('utf8');
+    res.on('data', chunk => {
+      body += chunk;
+    });
+    res.on('end', () => {
+      callback(null, JSON.parse(body));
+    })
+  });
+
+  req.on('error', (e) => {
+    callback(e, null);
+  });
+
+  req.end();
+};
+
+module.exports = Guitarparty;
