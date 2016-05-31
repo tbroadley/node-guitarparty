673055ff6b3ef3352bcedf07d76fda2b96a95105
tbroadley <buriedunderbooks@hotmail.com>
2016-05-31 11:56:54 +0100

    added caching functionality

--- guitarparty.js
+++ guitarparty.js
@@ -1,10 +1,18 @@
 const http = require('http');
 
-function Guitarparty(apiKey) {
+function Guitarparty(apiKey, options = {}) {
   this.apiKey = apiKey;
+  this.cache = {};
+  this.cacheExpiry = options.cacheExpiry || 3000;
 };
 
 Guitarparty.prototype.request = function(endpoint, callback) {
+  let cacheEntry = this.cache[endpoint];
+
+  if (cacheEntry && (Date.now() - cacheEntry.timestamp) / 1000 < this.cacheExpiry) {
+    callback(null, this.cache[endpoint].data);
+  }
+
   const options = {
     hostname: 'api.guitarparty.com',
     path: `/v2/${endpoint}`,
@@ -21,7 +29,14 @@ Guitarparty.prototype.request = function(endpoint, callback) {
       body += chunk;
     });
     res.on('end', () => {
-      callback(null, JSON.parse(body));
+      const data = JSON.parse(body);
+
+      cacheEntry = {
+        timestamp: Date.now(),
+        data,
+      };
+
+      callback(null, data);
     })
   });
 
